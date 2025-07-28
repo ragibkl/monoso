0f2aede51245e61512664f8c4cba79477e27f23c
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2025-07-29 00:42:51 +0800

    lookup zone via geodata

--- lib/hooks/zone.tsx
+++ lib/hooks/zone.tsx
@@ -1,14 +1,15 @@
 import { useCallback } from "react";
 
 import { zoneStore } from "@/lib/data/zoneStore";
-import { getZoneByGps, ZoneResponse } from "@/lib/remote/waktusolat";
+import { ZoneResponse } from "@/lib/remote/waktusolat";
+import { lookupZoneByGps } from "@/lib/service/zone";
 
 export function useZone() {
   const { data: zone, setData: setZone } = zoneStore.use();
 
   const updateZoneViaGps = useCallback(
-    async (lat: number, lng: number): Promise<ZoneResponse> => {
-      const zoneData = await getZoneByGps(lat, lng);
+    async (lat: number, lng: number): Promise<ZoneResponse | null> => {
+      const zoneData = lookupZoneByGps(lat, lng);
       setZone(zoneData);
 
       return zoneData;
--- lib/service/zone.ts
+++ lib/service/zone.ts
@@ -1,11 +1,36 @@
+import PolygonLookup from "polygon-lookup";
+
 import { zoneStore } from "@/lib/data/zoneStore";
-import { ZoneResponse, getZoneByGps } from "@/lib/remote/waktusolat";
+import { ZoneResponse } from "@/lib/remote/waktusolat";
+
+import jakimGeoData from "@/assets/geodata/malaysia-district-jakim.json";
+
+const lookup = new PolygonLookup(jakimGeoData as any);
+
+export function lookupZoneByGps(lat: number, lng: number): ZoneResponse | null {
+  const result = lookup.search(lng, lat);
+  console.log("lookupJakimZone", lat, lng, result);
+  if (!result || !result.properties) {
+    return null;
+  }
+
+  console.log("lookupJakimZone - properties", result.properties);
+  const zone = result.properties.jakim_code;
+  const state = result.properties.state;
+  const district = result.properties.name;
+
+  return {
+    zone,
+    state,
+    district,
+  };
+}
 
 export async function updateZoneViaGps(
   lat: number,
   lng: number,
 ): Promise<ZoneResponse> {
-  const zoneData = await getZoneByGps(lat, lng);
-  zoneStore.save(zoneData);
-  return zoneData;
+  const zoneData = lookupZoneByGps(lat, lng);
+  await zoneStore.save(zoneData);
+  return zoneData as ZoneResponse;
 }
