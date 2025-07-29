f30a3bb9995fa41e4ea64c63c319c1756a558ca3
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2025-07-29 12:34:16 +0800

    notif

--- lib/tasks/waktuSolatWidgetTask.tsx
+++ lib/tasks/waktuSolatWidgetTask.tsx
@@ -1,39 +1,101 @@
 import * as BackgroundTask from "expo-background-task";
+import * as Notifications from "expo-notifications";
 import * as TaskManager from "expo-task-manager";
 import React from "react";
 import { requestWidgetUpdate } from "react-native-android-widget";
 
+import { PrayerTime } from "@/lib/data/waktuSolatStore";
+import { zoneStore, Zone } from "@/lib/data/zoneStore";
 import { getLocation } from "@/lib/service/location";
 import { getOrRetrieveWaktuSolat } from "@/lib/service/waktuSolat";
-import { WaktuSolatWidget } from "@/lib/widgets/WaktuSolatWidget";
-import { zoneStore } from "@/lib/data/zoneStore";
 import { updateZoneViaGps } from "@/lib/service/zone";
+import { WaktuSolatWidget } from "@/lib/widgets/WaktuSolatWidget";
 
-const TASK_NAME = "update-waktu-solat-widget";
+const BG_TASK = "update-waktu-solat-widget";
+const NOTIF_TASK = "waktu-solat-notifications-task";
 
-TaskManager.defineTask(TASK_NAME, async () => {
+async function getUpdatedZone(): Promise<[Zone | null, boolean]> {
+  const prevZone = await zoneStore.load();
+
+  const location = await getLocation();
+  if (location) {
+    const zone = await updateZoneViaGps(
+      location.coords.latitude,
+      location.coords.longitude,
+    );
+
+    return [zone, prevZone?.zone !== zone.zone];
+  }
+
+  return [prevZone, false];
+}
+
+async function reRenderWidget(date: Date, zone: Zone, prayerTime: PrayerTime) {
+  await requestWidgetUpdate({
+    widgetName: "WaktuSolat",
+    renderWidget: () => (
+      <WaktuSolatWidget date={date} zone={zone} prayerTime={prayerTime} />
+    ),
+    widgetNotFound: () => {},
+  });
+}
+
+function getNextPrayerTime(
+  prayerTime: PrayerTime,
+  date: Date,
+): [string, number] | null {
+  const { fajr, syuruk, dhuhr, asr, maghrib, isha } = prayerTime;
+  const epoch = date.getTime() / 1000;
+
+  if (epoch < fajr) {
+    return ["fajr", fajr - epoch];
+  }
+
+  if (epoch < syuruk) {
+    return ["syuruk", syuruk - epoch];
+  }
+
+  if (epoch < dhuhr) {
+    return ["dhuhr", dhuhr - epoch];
+  }
+
+  if (epoch < asr) {
+    return ["asr", asr - epoch];
+  }
+
+  if (epoch < maghrib) {
+    return ["maghrib", maghrib - epoch];
+  }
+
+  if (epoch < isha) {
+    return ["isha", isha - epoch];
+  }
+
+  return null;
+}
+
+async function scheduleNextPrayerTime(prayerTime: PrayerTime, date: Date) {
+  const nextTime = getNextPrayerTime(prayerTime, date);
+  if (nextTime) {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: `Waktu Solat - ${nextTime[0]}`,
+      },
+      trigger: {
+        type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+        seconds: nextTime[1] + 10,
+        repeats: false,
+      },
+    });
+  }
+}
+
+TaskManager.defineTask(BG_TASK, async () => {
   try {
     const date = new Date();
     console.log(`${date.toISOString()}: Start background task`);
 
-    let zone = await zoneStore.load();
-    try {
-      console.log("Start zone update via GPS location");
-
-      const location = await getLocation();
-      if (location) {
-        const zoneData = await updateZoneViaGps(
-          location.coords.latitude,
-          location.coords.longitude,
-        );
-        if (zoneData) {
-          zone = zoneData;
-        }
-      }
-    } catch (error) {
-      console.log("Failed zone update via GPS location", error);
-    }
-
+    const [zone, updatedZone] = await getUpdatedZone();
     if (!zone) {
       console.log("Zone not set, returning");
       return BackgroundTask.BackgroundTaskResult.Success;
@@ -46,17 +108,17 @@ TaskManager.defineTask(TASK_NAME, async () => {
     }
 
     console.log("Found WaktuSolat, updating widget");
-    requestWidgetUpdate({
-      widgetName: "WaktuSolat",
-      renderWidget: () => (
-        <WaktuSolatWidget
-          date={date}
-          zone={zone}
-          prayerTime={waktuSolat.prayerTime}
-        />
-      ),
-      widgetNotFound: () => {},
-    });
+    await reRenderWidget(date, zone, waktuSolat.prayerTime);
+
+    if (updatedZone) {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+      await scheduleNextPrayerTime(waktuSolat.prayerTime, date);
+    } else {
+      const notifs = await Notifications.getAllScheduledNotificationsAsync();
+      if (!notifs.length) {
+        await scheduleNextPrayerTime(waktuSolat.prayerTime, date);
+      }
+    }
   } catch (error) {
     console.error("Failed background task:", error);
     return BackgroundTask.BackgroundTaskResult.Failed;
@@ -65,17 +127,46 @@ TaskManager.defineTask(TASK_NAME, async () => {
   return BackgroundTask.BackgroundTaskResult.Success;
 });
 
-export async function registerUpdateWaktuSolatWidgetTask() {
-  const status = await BackgroundTask.getStatusAsync();
-  console.log("BackgroundTask.getStatusAsync()", status);
+TaskManager.defineTask(NOTIF_TASK, async () => {
+  try {
+    const date = new Date();
+    console.log(`${date.toISOString()}: Start notif task`);
 
-  const registered = await TaskManager.isTaskRegisteredAsync(TASK_NAME);
-  console.log(`Task: ${TASK_NAME} - registered: ${registered}`);
+    const zone = await zoneStore.load();
+    if (!zone) {
+      console.log("Zone not set, returning");
+      return BackgroundTask.BackgroundTaskResult.Success;
+    }
 
-  if (!registered) {
-    console.log(`Task: ${TASK_NAME} - registerTaskAsync`);
-    await BackgroundTask.registerTaskAsync(TASK_NAME, {
-      minimumInterval: 15,
-    });
+    const waktuSolat = await getOrRetrieveWaktuSolat(zone.zone, date);
+    if (!waktuSolat) {
+      console.log("WaktuSolat not found, returning");
+      return BackgroundTask.BackgroundTaskResult.Success;
+    }
+
+    console.log("Found WaktuSolat, updating widget");
+    await reRenderWidget(date, zone, waktuSolat.prayerTime);
+  } catch (error) {
+    console.error("Failed background task:", error);
+    return BackgroundTask.BackgroundTaskResult.Failed;
   }
+
+  return BackgroundTask.BackgroundTaskResult.Success;
+});
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowBanner: true,
+    shouldShowList: false,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export async function registerUpdateWaktuSolatWidgetTask() {
+  await BackgroundTask.registerTaskAsync(BG_TASK, {
+    minimumInterval: 15,
+  });
+
+  Notifications.registerTaskAsync(NOTIF_TASK);
 }
