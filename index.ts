e5c32fbdd53973cdf08a7df76306d34b158c23d1
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2025-08-15 22:57:34 +0800

    setup notifee

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import "expo-router/entry";
 import { registerWidgetTaskHandler } from "react-native-android-widget";
 
-import "@/lib/tasks/backgroundTasks";
 import { widgetTaskHandler } from "@/lib/tasks/widgetTaskHandler";
+import "@/lib/tasks/backgroundTasks";
+import "@/lib/service/notifee";
+
+import "expo-router/entry";
 
 registerWidgetTaskHandler(widgetTaskHandler);
--- lib/service/notifee.ts
+++ lib/service/notifee.ts
@@ -1,7 +1,9 @@
-import * as Notifications from "expo-notifications";
+import notifee, { EventType, TriggerType } from "@notifee/react-native";
 
 import { WaktuSolat, PrayerTime } from "@/lib/data/waktuSolatStore";
 import { Zone } from "@/lib/data/zoneStore";
+import { updateWaktuSolatAndWidget } from "./waktuSolatWidget";
+import { requestWaktuSolatWidgetUpdate } from "../widgets/WaktuSolatWidget";
 
 export const WAKTU_SOLAT_PREFIX = "waktu_solat";
 
@@ -20,31 +22,11 @@ export function getEpochDate(epochSeconds: number): Date {
   return date;
 }
 
-export async function setAllWaktuSolatChannels() {
-  const waktu_keys = ["fajr", "syuruk", "dhuhr", "asr", "maghrib", "isha"];
-
-  for (const waktu of waktu_keys) {
-    const channelId = `${WAKTU_SOLAT_PREFIX}_${waktu}`;
-    await Notifications.setNotificationChannelAsync(channelId, {
-      name: waktu,
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-}
-
-export async function setupNotifications() {
-  await Notifications.requestPermissionsAsync();
-  await setAllWaktuSolatChannels();
-}
-
-export async function scheduleWaktuSolatNotification(
+async function scheduleWaktuSolatNotification(
   waktuSolat: WaktuSolat,
   zone: Zone,
   waktu: keyof PrayerTime,
 ) {
-  const channelId = `${WAKTU_SOLAT_PREFIX}_${waktu}`;
   const epochSeconds = waktuSolat.prayerTime[waktu];
   const date = getEpochDate(epochSeconds);
   const dateText = date.toLocaleString([], {
@@ -57,22 +39,34 @@ export async function scheduleWaktuSolatNotification(
     return;
   }
 
-  await Notifications.scheduleNotificationAsync({
-    content: {
+  // Request permissions (required for iOS)
+  await notifee.requestPermission();
+
+  // Create a channel (required for Android)
+  const channelId = await notifee.createChannel({
+    id: `${WAKTU_SOLAT_PREFIX}_${waktu}`,
+    name: waktu,
+  });
+
+  // Trigger a notification
+  await notifee.createTriggerNotification(
+    {
       title: `Waktu Solat - ${waktu} at ${dateText}`,
       body: `It is now ${waktu} in ${zone.district}, ${zone.state}`,
+      android: {
+        channelId,
+      },
       data: {
         waktuSolat,
         waktu,
         zone,
       },
     },
-    trigger: {
-      channelId,
-      date,
-      type: Notifications.SchedulableTriggerInputTypes.DATE,
+    {
+      type: TriggerType.TIMESTAMP,
+      timestamp: date.getTime(),
     },
-  });
+  );
 }
 
 export async function scheduleAllWaktuSolatNotifications(
@@ -90,22 +84,23 @@ export async function scheduleAllWaktuSolatNotifications(
     isha: false,
   };
 
-  const notifications = await Notifications.getAllScheduledNotificationsAsync();
+  const notifications = await notifee.getTriggerNotifications();
   for (const n of notifications) {
     // Assert notification trigger channel is waktu_solat_*
     if (
-      !n.trigger ||
-      !("channelId" in n.trigger) ||
-      !n.trigger.channelId?.startsWith("waktu_solat")
+      !n.notification.android?.channelId ||
+      !n.notification.android.channelId.startsWith("waktu_solat")
     ) {
       continue;
     }
 
     // Check if notification matches the current WaktuSolat zone and date
-    if (sameWaktuSolat(n.content.data.waktuSolat as WaktuSolat, waktuSolat)) {
-      existingNotifs[n.content.data.waktu as keyof PrayerTime] = true;
-    } else {
-      await Notifications.cancelScheduledNotificationAsync(n.identifier);
+    if (
+      sameWaktuSolat(n.notification?.data?.waktuSolat as WaktuSolat, waktuSolat)
+    ) {
+      existingNotifs[n.notification?.data?.waktu as keyof PrayerTime] = true;
+    } else if (n.notification.id) {
+      await notifee.cancelTriggerNotification(n.notification.id);
     }
   }
 
@@ -117,3 +112,17 @@ export async function scheduleAllWaktuSolatNotifications(
     }
   }
 }
+
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  if (type === EventType.DELIVERED) {
+    const data = detail?.notification?.data || {};
+
+    if (data && "waktuSolat" in data && "zone" in data) {
+      const { prayerTime } = data.waktuSolat as WaktuSolat;
+      const zone = data.zone as Zone;
+      await requestWaktuSolatWidgetUpdate(new Date(), zone, prayerTime);
+    } else {
+      await updateWaktuSolatAndWidget(false, false);
+    }
+  }
+});
